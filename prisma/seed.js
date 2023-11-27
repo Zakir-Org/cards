8f2d03ce41737b7dd517ddc9a849200dc406fd2a
Zakir Aghayev <agayevzakir239@gmail.com>
2023-11-27 21:20:37 +0400

    Done with api doc

--- prisma/seed.js
+++ prisma/seed.js
@@ -14,15 +14,34 @@ async function createCardWithDetails(cardData, cashbackData, limitsData) {
     });
 }
 
+function getRandomFloat(min, max, decimalPlaces = 2) {
+    return parseFloat((Math.random() * (max - min) + min).toFixed(decimalPlaces));
+}
+
 async function main() {
     for (let i = 1; i <= 5; i++) {
         await createCardWithDetails(
-            { bankOfCard: `Bank ${i}`, cardName: `Card ${i}`, cardType: 'Visa', interestYearly: 5.0 + i },
-            { fuel: 1.5 + i, store: 1.0 + i, grocers: 2.0 + i, other: 0.5 + i },
-            { card2Card: 1000 * i, interestCalc: 200 * i, maxAmount: 10000 * i, withdraw: 500 * i },
+            {
+                bankOfCard: `Bank ${i}`,
+                cardName: `Card ${i}`,
+                cardType: 'Visa',
+                interestYearly: getRandomFloat(1.0, 10.0),
+            },
+            {
+                fuel: getRandomFloat(0.5, 5.0),
+                store: getRandomFloat(0.5, 5.0),
+                grocers: getRandomFloat(0.5, 5.0),
+                other: getRandomFloat(0.5, 5.0),
+            },
+            {
+                card2Card: getRandomFloat(500, 5000),
+                interestCalc: getRandomFloat(100, 1000),
+                maxAmount: getRandomFloat(5000, 50000),
+                withdraw: getRandomFloat(100, 2000),
+            },
         );
     }
-    console.log('5 cards with related cashback and limits have been created.');
+    console.log('5 cards with related cashback and limits have been created with random values.');
 }
 
 main()
